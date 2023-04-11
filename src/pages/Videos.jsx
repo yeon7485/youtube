810520d38156576e0b508f7ce92d26797bdd43bf
박세연 <wsx4379@naver.com>
2023-04-12 00:48:08 +0900

    first commit

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+
+export default function Videos() {
+    const { keyword } = useParams();
+    const { youtube } = useYoutubeApi();
+
+    const {
+        isLoading,
+        error,
+        data: videos,
+    } = useQuery(["videos", keyword], () => youtube.search(keyword), {
+        staleTime: 1000 * 60 * 1,
+    });
+
+    return (
+        <div>
+            Videos {keyword ? `${keyword}` : "hot"}
+            {isLoading && <p> Loading....</p>}
+            {error && <p>Something is wrong </p>}
+            {videos && (
+                <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
+                    {videos.map((video) => (
+                        <VideoCard video={video} />
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
